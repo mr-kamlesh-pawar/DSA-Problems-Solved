fcfab6aee0663a279068a36ff9cb2e27848f83ae
Kamlesh Pawar <kamleshpawar5278@gmail.com>
2024-02-01 23:55:27 +0530

    New

---
+++ Solved Problems/StackUsingQueue.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+#include<queue>
+
+class MyStack {
+public:
+queue<int> q1,q2;
+    MyStack() {
+ 
+        
+    }
+    
+    void push(int x) {
+        while(!q1.empty()){
+            q2.push(q1.front());
+            q1.pop();
+        }
+
+        q1.push(x);
+        while(!q2.empty()){
+            q1.push(q2.front());
+            q2.pop();
+        }
+        
+    }
+    
+    int pop() {
+        int val= q1.front();
+        q1.pop();
+        return val;
+    }
+    
+    int top() {
+        return q1.front();
+        
+    }
+    
+    bool empty() {
+   return q1.empty();     
+    }
+};
+
+
+int main(){
+MyStack my;
+cout<<"Ener a how many Element: ";
+cin>>x;
+
+
+    return 0;
+}
