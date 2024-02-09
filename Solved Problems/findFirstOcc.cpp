11b444417e13ecf237711577fec6d03e884c861a
Kamlesh Pawar <kamleshpawar5278@gmail.com>
2024-02-09 23:34:25 +0530

    Last word length

--- Solved Problems/findFirstOcc.cpp
+++ Solved Problems/findFirstOcc.cpp
@@ -1,24 +1,56 @@
-#include<iostream>
-#include<string>
+#include <iostream>
+#include <string>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    int strStr(string haystack, string needle) {
-        for(int i=0;i<haystack.size();i++) {
-            string temp = haystack.substr(i, needle.size());
-            if(temp == needle)
-            { return i;
+    // int strStr(string haystack, string needle) {
+    //     for(int i=0;i<haystack.size();i++) {
+    //         string temp = haystack.substr(i, needle.size());
+    //         if(temp == needle)
+    //         { return i;
 
+    //         }
+    //     }
+    //     return -1;
+    // }
+
+    int strStr(string haystack, string needle)
+    {
+        string temp = "";
+        
+        for (int i = 0; i < needle.length(); i++)
+        {
+            if (haystack[i] == needle[0])
+            {
+               
+                    for (int j = 0; j < haystack.length(); j++)
+                    {
+                        int count = 0;
+                        if (haystack[j] == needle[count])
+                        {
+                            count++;
+                            temp += haystack[j];
+                        }
+                    }
+                
+                    if (temp == needle)
+                    {
+                        return i;
+                    }
+                
+            
             }
         }
-        return -1;
+        return 0;
     }
 };
 
-int main(){
+int main()
+{
     Solution sol;
-    cout<<sol.strStr("a","a");
+    cout << sol.strStr("textbook", "book");
 
     return 0;
 }
---
+++ Solved Problems/lengthLastWord.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+class Solution{
+
+    public:
+    int lastwordlength(string s){
+        int strlen= s.size();
+        int i=strlen-1;
+        int count=0;
+
+        while(i>=0 && s[i]==' '){
+        i--; }
+
+        while(i>=0 && s[i]!=' '){
+            count++;
+            i--;
+        }
+
+        return count;
+    }
+
+}sol;
+
+int main(){
+    string ss= "  hello world   ";
+    cout<<sol.lastwordlength(ss);
+    return 0;
+}
