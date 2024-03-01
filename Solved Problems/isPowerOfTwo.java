1f8e715b7afcc9264744d72910c1be9aa4ffb708
Kamlesh Pawar <kamleshpawar5278@gmail.com>
2024-03-01 23:30:20 +0530

    Is Power of Two

---
+++ Solved Problems/isPowerOfTwo.java
@@ -0,0 +1,21 @@
+public class isPowerOfTwo {
+
+    boolean isPowoftwo(int n){
+        if(n==0)
+            return false;
+        
+            while(n>1 && (n&1)==0){
+                n>>=1;
+            } return n==1?true:false;
+    }
+    public static void main(String args[]){
+        isPowerOfTwo ip= new isPowerOfTwo();
+        System.out.println("HHHHH");
+        System.out.println("To Check the power odf 2 is or not: ");
+        int n=1;
+        while(n<=10){
+            System.out.println("The no is a "+n+" : "+ ip.isPowoftwo(n));
+            n++;
+        }
+    }
+}
