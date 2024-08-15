ed9c9756f386957b4dea0bf4cc51973ba558fd83
Kamlesh Pawar <kamleshpawar5278@gmail.com>
2024-08-15 22:47:37 +0530

    toLower

---
+++ Solved Problems/toLower.cpp
@@ -0,0 +1,14 @@
+class Solution
+{
+public:
+    string toLowerCase(string s)
+    {
+
+        for (int i = 0; i < s.size(); i++)
+        {
+            if (s[i] <= 90 && s[i] >= 65)
+                s[i] = s[i] + 32;
+        }
+        return s;
+    }
+};
