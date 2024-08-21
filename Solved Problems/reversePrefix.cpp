0966584a6f8cfc2fee3f204869f89155bb77ffc6
Kamlesh Pawar <kamleshpawar5278@gmail.com>
2024-08-21 23:39:15 +0530

    new

---
+++ Solved Problems/reversePrefix.cpp
@@ -0,0 +1,18 @@
+class Solution
+{
+public:
+    string reversePrefix(string word, char ch)
+    {
+        string str = "";
+
+        for (int i = 0; i < word.size(); i++)
+        {
+            if (word[i] == ch)
+            {
+                reverse(word.begin(), word.begin() + i + 1);
+                break;
+            }
+        }
+        return word;
+    }
+};
