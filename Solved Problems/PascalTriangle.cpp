772035caab163e42bc2cba5d4e58058f72cd3ff4
Kamlesh Pawar <kamleshpawar5278@gmail.com>
2024-09-15 04:10:51 +0530

    Pascal's Triangle

---
+++ Solved Problems/PascalTriangle.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> gnerateRow(int row){
+        long long ans=1;
+        vector<int> ansrow;
+        ansrow.push_back(1);
+
+        for(int col=1; col<row; col++){
+            ans= ans *(row-col);
+            ans= ans/col;
+            ansrow.push_back(ans);
+
+        }
+        return ansrow;
+
+    }
+
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> ans;
+        for(int i=1; i<=numRows; i++){
+            ans.push_back(gnerateRow(i));
+
+        }
+        return ans;
+    }
+};
