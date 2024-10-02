37f81fa3054fcc5d17aad8b354746c618425cf49
Kamlesh Pawar <kamleshpawar5278@gmail.com>
2024-10-02 22:28:51 +0530

    removeOuterParentheses

--- Solved Problems/removeOuterParentheses.cpp
+++ Solved Problems/removeOuterParentheses.cpp
@@ -1,29 +1,30 @@
-class Solution {
+class Solution
+{
 public:
-    string removeOuterParentheses(string s) {
-       int count=0;
-      string  str="";
-        for(int i=0; i<s.size(); i++){
+    string removeOuterParentheses(string s)
+    {
+        int count = 0;
+        string str = "";
+        for (int i = 0; i < s.size(); i++)
+        {
 
-            if(s[i] == '('){
-               if(count>0){
-                str+=s[i];
-
-               }
-                count++;
-
-            }else{
-                count--;
-                if(count>0){
-                    str+=s[i];
-
+            if (s[i] == '(')
+            {
+                if (count > 0)
+                {
+                    str += s[i];
                 }
-
+                count++;
             }
-
-
+            else
+            {
+                count--;
+                if (count > 0)
+                {
+                    str += s[i];
+                }
+            }
         }
         return str;
-        
     }
 };
